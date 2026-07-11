6ac1e1744d9f98fe00c00eaf5b93ecd14cfc9397
AmanKumar1612 <amankumar16122003@gmail.com>
2026-07-11 17:52:54 +0530

    Time: 27 ms (90.99%), Space: 19.7 MB (90.53%) - LeetHub

---
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -0,0 +1,42 @@
+from typing import List
+
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            
+        vis = [False] * n
+        complete_components = 0
+        
+        for i in range(n):
+            if not vis[i]:
+                queue = [i]
+                vis[i] = True
+                
+                component_nodes = []
+                head = 0
+                
+                while head < len(queue):
+                    curr = queue[head]
+                    head += 1
+                    component_nodes.append(curr)
+                    
+                    for neighbor in adj[curr]:
+                        if not vis[neighbor]:
+                            vis[neighbor] = True
+                            queue.append(neighbor)
+                
+                num_nodes = len(component_nodes)
+                is_complete = True
+                
+                for node in component_nodes:
+                    if len(adj[node]) != num_nodes - 1:
+                        is_complete = False
+                        break
+                        
+                if is_complete:
+                    complete_components += 1
+                    
+        return complete_components
